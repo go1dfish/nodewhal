e734885317911795479e28710b057e588d5535f9
Patrick Shanahan <ankhmorporkian@redditanalytics.com>
2013-12-05 16:13:10 -0600

    Fixed up some function signatures that were still using session.

--- nodewhal.js
+++ nodewhal.js
@@ -1,10 +1,10 @@
-var request  = require('request'),
-    RSVP     = require('rsvp'),
-    schedule = require('./schedule'),
-    baseUrl  = 'http://www.reddit.com',
-    knownShadowbans = {},
-    lastRedditRequestTimeByUrl = {},
-    lastRedditRequestTime;
+var request = require('request'),
+  RSVP = require('rsvp'),
+  schedule = require('./schedule'),
+  baseUrl = 'http://www.reddit.com',
+  knownShadowbans = {},
+  lastRedditRequestTimeByUrl = {},
+  lastRedditRequestTime;
 
 function Nodewhal(userAgent) {
   var self = this;
@@ -12,87 +12,90 @@ function Nodewhal(userAgent) {
   if (!userAgent) {
     userAgent = 'noob-nodewhal-dev-soon-to-be-ip-banned';
   }
-  self.login = function(username, password) {
+  self.login = function (username, password) {
     var cookieJar = request.jar();
     return self.post(baseUrl + '/api/login', {
       form: {
         api_type: 'json',
-        passwd:   password,
-        rem:      true,
-        user:     username
+        passwd: password,
+        rem: true,
+        user: username
       }
-    }, {cookieJar: cookieJar}).then(function(data) {
-      self.session =  data.json.data;
-      self.session.cookieJar = cookieJar;
-      return self;
-    });
+    }, {cookieJar: cookieJar}).then(function (data) {
+        self.session = data.json.data;
+        self.session.cookieJar = cookieJar;
+        return self;
+      });
   };
 
-  self.submit = function(subreddit, kind, title, urlOrText) {
+  self.submit = function (subreddit, kind, title, urlOrText) {
     urlOrText = urlOrText || '';
-    kind = (kind || 'link').toLowerCase(); var form = {
-        api_type: 'json',
-        kind:     kind,
-        title:    title,
-        sr:       subreddit,
-        uh:       self.session.modhash
+    kind = (kind || 'link').toLowerCase();
+    var form = {
+      api_type: 'json',
+      kind: kind,
+      title: title,
+      sr: subreddit,
+      uh: self.session.modhash
     };
     console.log('Submitting', urlOrText);
-    if (kind === 'self' || ! urlOrText) {
+    if (kind === 'self' || !urlOrText) {
       form.text = urlOrText;
     } else {
       form.url = urlOrText;
     }
-    return self.post(baseUrl + '/api/submit', {form: form}, self.session).then(function(data) {
+    return self.post(baseUrl + '/api/submit', {form: form}).then(function (data) {
       if (data && data.json && data.json.errors && data.json.errors.length) {
         throw data.json.errors;
       }
-      if (data && data.json && data.json.data) {return data.json.data;}
+      if (data && data.json && data.json.data) {
+        return data.json.data;
+      }
       return data;
     });
   };
 
-  self.comment = function(thing_id, markdown) {
+  self.comment = function (thing_id, markdown) {
     return self.post(baseUrl + '/api/comment', {
       form: {
         api_type: 'json',
-        text:     markdown,
+        text: markdown,
         thing_id: thing_id,
-        uh:       self.session.modhash
+        uh: self.session.modhash
       }
-    }, self.session);
+    });
   };
 
-  self.flair = function(subreddit, linkName, template, flairText) {
+  self.flair = function (subreddit, linkName, template, flairText) {
     return self.post(baseUrl + '/api/flair', {
       form: {
-        api_type:           'json',
-        link:               linkName,
-        r:                  subreddit,
-        text:               flairText,
-        css_class:          template,
-        uh:                 self.session.modhash
+        api_type: 'json',
+        link: linkName,
+        r: subreddit,
+        text: flairText,
+        css_class: template,
+        uh: self.session.modhash
       }
-    }, self.session)
+    })
   };
 
-  self.aboutUser = function(username) {
-    return self.get(baseUrl + '/user/' + username + '/about.json', {}, self.session);
+  self.aboutUser = function (username) {
+    return self.get(baseUrl + '/user/' + username + '/about.json', {});
   };
 
-  self.submitted = function(subreddit, url) {
+  self.submitted = function (subreddit, url) {
     url = encodeURIComponent(url);
-    return self.get(baseUrl + '/r/' + subreddit + '/submit.json?url=' + url, {}, self.session);
+    return self.get(baseUrl + '/r/' + subreddit + '/submit.json?url=' + url, {});
   };
 
-  self.checkForShadowban = function(username) {
+  self.checkForShadowban = function (username) {
     var url = baseUrl + '/user/' + username;
-    return Nodewhal.respectRateLimits('get', url).then(function() {
-      return new RSVP.Promise(function(resolve, reject) {
+    return Nodewhal.respectRateLimits('get', url).then(function () {
+      return new RSVP.Promise(function (resolve, reject) {
         if (knownShadowbans[username]) {
           return resolve('shadowban');
         }
-        request(url, {}, function(error, response, body) {
+        request(url, {}, function (error, response, body) {
           if (error) {
             reject(error);
           } else {
@@ -108,41 +111,41 @@ function Nodewhal(userAgent) {
     });
   };
 
-  self.listing = function(listingPath, options) {
+  self.listing = function (listingPath, options) {
     var url = baseUrl + listingPath + '.json',
-        options = options || {},
-        max = options.max,
-        after = options.after,
-        limit = max || 100;
+      options = options || {},
+      max = options.max,
+      after = options.after,
+      limit = max || 100;
     if (after) {
       url += '?limit=' + limit + '&after=' + after;
     }
-    return self.get(url, {}, self.session).then(function(listing) {
+    return self.get(url, {}).then(function (listing) {
       var results = {}, resultsLength;
       if (listing && listing.data && listing.data.children && listing.data.children.length) {
-        listing.data.children.forEach(function(submission) {
+        listing.data.children.forEach(function (submission) {
           results[submission.data.name] = submission.data;
         });
         resultsLength = Object.keys(results).length;
 
         if (
           listing.data.after &&
-          (typeof max === 'undefined' || resultsLength < max)
-        ) {
+            (typeof max === 'undefined' || resultsLength < max)
+          ) {
           if (!typeof max === 'undefined') {
             max = max - resultsLength;
           }
-          return schedule.wait(options.wait).then(function() {
-            return self.listing(self.session, listingPath, {
+          return schedule.wait(options.wait).then(function () {
+            return self.listing(listingPath, {
               max: max,
               after: listing.data.after,
               wait: options.wait
-            }).then(function(moreResults) {
-              Object.keys(moreResults).forEach(function(key) {
-                results[key] = moreResults[key];
-              });
-              return results;
-            })
+            }).then(function (moreResults) {
+                Object.keys(moreResults).forEach(function (key) {
+                  results[key] = moreResults[key];
+                });
+                return results;
+              })
           });
         } else {
           return results;
@@ -154,8 +157,8 @@ function Nodewhal(userAgent) {
   };
 
   self.byId = function (ids) {
-    ids = ids.map(function(id) {
-      if (id.substr(0,3) == "t3_") {
+    ids = ids.map(function (id) {
+      if (id.substr(0, 3) == "t3_") {
         return id
       }
       else {
@@ -164,29 +167,28 @@ function Nodewhal(userAgent) {
     });
     console.log("Fetching submissions.");
     var url = baseUrl + "/by_id/" + ids.join(",") + '/.json';
-    return self.get(url, {}, self.session).then(function (listing) {
-      var results = {}, resultsLength;
+    return self.get(url, {}).then(function (listing) {
+      var results = {};
       if (listing && listing.data && listing.data.children && listing.data.children.length) {
         listing.data.children.forEach(function (submission) {
           results[submission.data.name] = submission.data;
         });
-        resultsLength = Object.keys(results).length;
       }
       return results;
     });
 
   };
 
-  self.get = function(url, opts) {
-    return self.req(url, 'get', opts, self.session);
+  self.get = function (url, opts) {
+    return self.req(url, 'get', opts);
   };
 
-  self.post = function(url, opts) {
-    return self.req(url, 'post', opts, self.session);
+  self.post = function (url, opts) {
+    return self.req(url, 'post', opts);
   };
 
-  self.req = function(url, method, opts) {
-    return Nodewhal.respectRateLimits(method, url).then(function() {
+  self.req = function (url, method, opts) {
+    return Nodewhal.respectRateLimits(method, url).then(function () {
       opts = opts || {};
       if (self.session && self.session.cookieJar) {
         opts.jar = self.session.cookieJar;
@@ -194,23 +196,25 @@ function Nodewhal(userAgent) {
       opts.headers = opts.headers || {};
       opts.headers['User-Agent'] = userAgent;
       return Nodewhal.rsvpRequest(method, url, opts);
-    }).then(function(body) {
-      var json;
-      try {
-        json = JSON.parse(body);
-      } catch(e) {
-        console.error('Cant parse', body);
-        throw e;
-      }
-      if (json && json.error) {
-        console.log('error', json);
-        throw json.error;
-      }
-      return json;
-    }, function(error) {
-      if (error.stack) {console.error(error.stack);}
-      throw error;
-    });
+    }).then(function (body) {
+        var json;
+        try {
+          json = JSON.parse(body);
+        } catch (e) {
+          console.error('Cant parse', body);
+          throw e;
+        }
+        if (json && json.error) {
+          console.log('error', json);
+          throw json.error;
+        }
+        return json;
+      }, function (error) {
+        if (error.stack) {
+          console.error(error.stack);
+        }
+        throw error;
+      });
   };
 }
 
@@ -235,23 +239,23 @@ Nodewhal.rsvpRequest = function (method, url, opts) {
 };
 
 Nodewhal.respectRateLimits = function (method, url) {
-  return new RSVP.Promise(function(resolve, reject) {
+  return new RSVP.Promise(function (resolve, reject) {
     var now = new Date(),
-        minInterval = 2100,
-        minUrlInterval = 30100,
-        lastUrlInterval, lastUrlTime = lastRedditRequestTimeByUrl[url],
-        interval = now - lastRedditRequestTime;
+      minInterval = 2100,
+      minUrlInterval = 30100,
+      lastUrlInterval, lastUrlTime = lastRedditRequestTimeByUrl[url],
+      interval = now - lastRedditRequestTime;
 
     if (method == 'get' && lastUrlTime) {
       lastUrlInterval = now - lastUrlTime;
     }
     if (lastRedditRequestTime && interval < minInterval) {
-      resolve(schedule.wait(minInterval - interval).then(function() {
+      resolve(schedule.wait(minInterval - interval).then(function () {
         return Nodewhal.respectRateLimits(method, url);
       }));
     } else {
       if (lastUrlInterval && lastUrlInterval < minUrlInterval) {
-        resolve(schedule.wait(minUrlInterval - lastUrlInterval).then(function() {
+        resolve(schedule.wait(minUrlInterval - lastUrlInterval).then(function () {
           return Nodewhal.respectRateLimits(method, url);
         }));
       } else {
@@ -260,12 +264,12 @@ Nodewhal.respectRateLimits = function (method, url) {
         resolve(true);
       }
     }
-  }).then(undefined, function(error) {
-    if (error.stack) {
-      console.error(error.stack);
-    }
-    throw error;
-  });
+  }).then(undefined, function (error) {
+      if (error.stack) {
+        console.error(error.stack);
+      }
+      throw error;
+    });
 };
 
 module.exports = Nodewhal;
